5536a2d46ceb69c00a82910cb60ead7e52f938fe
Wojciech Nawrocki <wjn@lean-fro.org>
2026-04-13 17:38:49 -0400

    feat: ts settings

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
+  turbopack: {
+    // Without this, Turbopack uses the parent dir as root
+    root: import.meta.dirname
+  }
 };
 
 export default nextConfig;
