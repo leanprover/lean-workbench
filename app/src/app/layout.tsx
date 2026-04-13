0861527c3c531868723e9364cfab3f241beeb3eb
Wojciech Nawrocki <wjn@lean-fro.org>
2026-04-13 17:29:19 -0400

    feat: scaffold

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Lean Workbench",
+  description: "Web platform for Lean",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
