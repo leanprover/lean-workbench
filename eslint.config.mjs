43e2717782eef746b3ccae3eb8f25fc83e1523c7
Rob Simmons <rob@lean-fro.org>
2026-07-03 13:50:07 -0400

    chore: standardize on '@/' style paths
    enforced in vscode settings, checked in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
 import prettier from 'eslint-config-prettier/flat'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
@@ -19,6 +20,16 @@ const eslintConfig = defineConfig([
     'next-env.d.ts',
     'branch-*/',
   ]),
+  {
+    files: ['src/**/*.{mtx,ts,tsx}'],
+    plugins: { 'no-relative-import-paths': noRelativeImportPaths },
+    rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'warn',
+        { allowSameFolder: true, rootDir: 'src', prefix: '@' },
+      ],
+    },
+  },
 ])
 
 export default eslintConfig
