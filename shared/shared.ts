6aba24db62b97a2366fc733d1badbeded44eaa56
Rob Simmons <robsimmons@gmail.com>
2026-08-20 21:02:30 -0400

    update shared.ts

--- shared/shared.ts
+++ shared/shared.ts
@@ -52,4 +52,30 @@ export const zWorkspaceMetadata = z.object({
   excludeSyncPatterns: z.array(z.string()).optional(),
 })
 
-export * from './consts.ts'
+/** We keep a Y.Doc per collaboratively-editable file.
+ * This is the Y.Doc key under which the text content lives. */
+export const YTEXT_KEY = 'content'
+
+/** Name of the `collab-server` database file. */
+export const COLLAB_DB_FILENAME = 'collab.db'
+
+/** Name of the `collab-server` UDS file. */
+export const COLLAB_SOCKET_FILENAME = 'collab.sock'
+
+/** 
+ * Where bwrap mounts the given project directory.
+ * We identify project files by absolute path in Yjs,
+ * so this has to match across VS Code server and collab-server bwraps.
+ */
+export function bwrapProjectDir(projectName: string) {
+  return `/workspace/${projectName}/`
+}
+
+/** Path to workspace metadata file in VSCode bwraps. */
+export const BWRAP_METADATA_PATH = '/workspace/.lean-workbench.json'
+
+/** Working directory of collab-server in the VSCode and collab-server bwraps. */
+export const BWRAP_COLLAB_SERVER_DIR = '/workspace/.collab-server'
+
+/** Collab-server socket path in the VSCode and collab-server bwraps. */
+export const BWRAP_COLLAB_SOCK_PATH = `${BWRAP_COLLAB_SERVER_DIR}/${COLLAB_SOCKET_FILENAME}`
