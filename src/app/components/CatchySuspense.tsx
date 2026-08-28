11c02729fefeb3aa013ce850f86eb64587238862
Rob Simmons <robsimmons@gmail.com>
2026-08-28 18:12:41 -0400

    refactor: improve CatchySuspense

--- src/app/components/CatchySuspense.tsx
+++ src/app/components/CatchySuspense.tsx
@@ -6,34 +6,34 @@ import z from 'zod'
 
 import ErrorBox from './ErrorBox'
 
-function ErrorFallback(props: { fallback?: (err: unknown) => ReactNode }, errorInfo: ErrorInfo) {
-  console.log(errorInfo)
-  const parsedError = z
-    .object({ digest: z.string().optional(), message: z.string().optional() })
-    .safeParse(errorInfo.error)
-  const message = props.fallback ? (
-    props.fallback(errorInfo.error)
-  ) : !parsedError.success ? (
-    'Something unexpected went wrong!'
-  ) : parsedError.data.digest ? (
-    process.env.NODE_ENV === 'production' ? (
-      `A server-side error occurred (digest ${parsedError.data.digest})`
-    ) : (
-      <>
-        <div>Server-side error, in production users will only see the digest {parsedError.data.digest}.</div>
-        <div>{parsedError.data.message}</div>
-      </>
-    )
-  ) : (
-    (parsedError.data.message ?? 'Something unexpected went wrongs!')
-  )
+function ErrorFallback(props: { fallback?: ReactNode | ((err: unknown) => ReactNode) }, { error, retry }: ErrorInfo) {
+  let displayedError: ReactNode
+  if (props.fallback !== undefined) {
+    displayedError = typeof props.fallback === 'function' ? props.fallback(error) : props.fallback
+  } else {
+    try {
+      const { digest, message } = z.object({ digest: z.string(), message: z.string().optional() }).parse(error)
+      displayedError =
+        process.env.NODE_ENV === 'production' ? (
+          `A server-side error occurred (digest ${digest})`
+        ) : (
+          <>
+            <div>Server-side error, in production users will only see the digest {digest}.</div>
+            <div>{message}</div>
+          </>
+        )
+    } catch {
+      // Presumably a client-side error, parse more like a usual caught exception
+      displayedError = error instanceof Error ? error.message : String(error)
+    }
+  }
 
   return (
     <ErrorBox>
       <div style={{ display: 'flex', gap: 8, flexDirection: 'column' }}>
-        {message}
+        {displayedError}
         <div>
-          <button onClick={errorInfo.retry}>Retry</button>
+          <button onClick={retry}>Retry</button>
         </div>
       </div>
     </ErrorBox>
@@ -45,16 +45,18 @@ const ErrorBoundary = catchError(ErrorFallback)
 /**
  * Combines a `<Suspense>` boundary with an error boundary derived from Next.js's `catchError`.
  *
- * The `loading` prop works exactly the Suspense component's `fallback`
+ * The `loading` fallback works exactly the Suspense component's `fallback`
  * and is inserted directly into the document.
  *
- * The error message is derived from the thrown error, via the `error` parameter if it is provided.
- * Keep in mind that, in most cases, the actual thrown error will originate from the server
+ * The `error` fallback can be a ReactNode or a function producing a React node from the actual error.
+ * (Note: if the <CatchySuspense> is in a server component, the error fallback CANNOT be a function.)
+ *
+ * Keep in mind that, in production environments, the actual thrown error may originate from the server
  * and so will be an unhelpful digest string with a generic "An error occurred in the Server Components blah blah blah" message.
  */
 export default function CatchySuspense(props: {
   loading: ReactNode
-  error?: (err: unknown) => ReactNode
+  error?: ReactNode | ((err: unknown) => ReactNode)
   children: ReactNode
 }) {
   return (
